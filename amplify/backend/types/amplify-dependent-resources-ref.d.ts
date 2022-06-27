1f76ed5be15693e9e84b30a7f4ea6a940f3364a1
BadBeatBuck <94531969+BadBeatBuck@users.noreply.github.com>
2022-06-27 01:06:49 -0700

    delete amplify files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "demoreactapp": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
