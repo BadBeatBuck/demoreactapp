a21eaddae37bbf75de1cab4c7dd77280d6d0e489
BadBeatBuck <94531969+BadBeatBuck@users.noreply.github.com>
2022-06-27 01:31:11 -0700

    clean up files

--- client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "client": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
