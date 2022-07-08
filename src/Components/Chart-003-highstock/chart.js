5d274d544efda08fbd5ad93956e2df96dbb49b47
BadBeatBuck <94531969+BadBeatBuck@users.noreply.github.com>
2022-07-07 23:47:56 -0700

    add useRef to chart

--- src/Components/Chart-003-highstock/chart.js
+++ src/Components/Chart-003-highstock/chart.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useRef, useEffect } from "react";
+
 import HighStock from "highcharts/highstock";
 import HighchartsReact from "highcharts-react-official";
 import Indicators from "highcharts/indicators/indicators-all.js";
@@ -13,6 +15,10 @@ import cx from "classnames";
 import css from "./chart.module.scss";
 import getOptions from "./options";
 
+// const [options] = useState({...});
+
+// return <HighchartsReact ref={chartComponent} highcharts={Highcharts} options={options} />;
+
 function Chart003(props = {}) {
   Indicators(HighStock);
   DragPanes(HighStock);
@@ -22,13 +28,17 @@ function Chart003(props = {}) {
   StockTools(HighStock);
   Boost(HighStock);
 
+  const chartComponent = useRef(null);
+
+  useEffect(() => {
+    const chart = chartComponent.current.chart;
+  }, []);
+
   const options2 = props.options || {};
 
   const className = cx(css.main, {
     [props.className]: !!props.className,
   });
-
-  console.log("gl", props.gridLines);
 
   const combinedOptions = {
     ...getOptions({ data: props.data || [], gridLines: props.gridLines }),
@@ -38,6 +48,7 @@ function Chart003(props = {}) {
   return (
     <div className={className}>
       <HighchartsReact
+        ref={chartComponent}
         highcharts={HighStock}
         constructorType="stockChart"
         options={combinedOptions}
